ead591037b7463c2db8e9cd4d4b8345254ed3319
Owen Riske <owenr0129@5565.ca>
2022-12-20 08:25:57 -0700

    Initial commit

--- src/com/company/books.java
+++ src/com/company/books.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.io.File;  // Import the File class
+
 
 public class books {
     String bookName;
@@ -11,10 +13,14 @@ public class books {
         description=description;
         genre=genre;
 
-
+    }
     void changeRating(int newRating){
-
         rating=newRating;
-        }
+    }
+    void makeBook(String bookName){
+        File myObj = new File(bookName);
+    }
+    void saveInfo( String description, String[] genre, int rating){
+
     }
 }
